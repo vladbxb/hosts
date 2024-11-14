55acd649811d6fe84b8db8e958482212a5e6a5f9
vladbxb <vladbxb@gmail.com>
2024-11-14 11:17:42 -0500

    commit 2: scriptul modificat pentru a primi ca parametrii un hostname, un ip si un server dns in loc sa verifice valididatea adreselor din /etc/hosts

--- bogus_host.sh
+++ bogus_host.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-nslookup $1 | while read linie; do
+nslookup $1 $3 | while read linie; do
 	if echo "$linie" | grep -q "Name:*"; then
 		read address_line
 		read label good_ip <<< "$address_line"
 		if [ $2 != $good_ip ]; then
+			#echo "$2"
+			#echo "$good_ip"
 			echo "Bogus IP for $1 !"
 		fi
 		break
